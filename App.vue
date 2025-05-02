4a72b14b0a8a7678d65ad21a9b1b303f9bc1b9e2
Hug1nn <118984296+YSY0426@users.noreply.github.com>
2025-05-02 20:37:46 +0800

    Add files via upload
    new layout

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+// 这里保持你原来的逻辑不变
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+/* 先加载全局 Reset + 主题变量 */
+@import "./styles/reset.less";
+/* 原有 App.vue 样式，可留可删 */
+#app {
+  /* 如果你想让页面撑满全屏高度，保留下面这一行 */
+  height: 100vh;
+}
+</style>
